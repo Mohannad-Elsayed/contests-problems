632a8cf1a5fe0ada31cfbc3c01da38e8361d7a89
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-03-30 14:35:57 +0200

    atcoder abc347

---
+++ Atcoder/abc347/abc347_a.cpp
@@ -0,0 +1,45 @@
+// A - Divisible
+// AtCoder - AtCoder Beginner Contest 347
+// https://atcoder.jp/contests/abc347/tasks/abc347_a
+// 2024-03-30 14:03:51
+
+#include<bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define each(a, x) for (auto &a : x)
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define F first
+#define S second
+#define pb emplace_back
+#define pf emplace_front
+#define lwb lower_bound
+#define upb upper_bound
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long llu;
+typedef short int si;
+#define forn(i,a,b) for(llu i=(llu)(a);i<(llu)(b);i++)
+#define rofn(i,a,b) for(llu i=(llu)(a);i>(llu)(b);i--)
+const ll MOD {1000000007};
+si solve(){
+    int n, k, t; cin >> n >> k;
+    while(n--){
+    	cin >> t;
+    	if (t%k) continue;
+    	cout << t/k << ' ';
+    }
+    return 0;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int32_t t = 1;
+    // cin >> t;
+    forn(i, 1, t+1){
+    	// cout << "# " << i << '\n';
+      	solve();
+      	cout << '\n';
+    }
+    return 0;
+}

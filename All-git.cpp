c99fa5d62ff5f92790ab8c6ae582cd617e44958d
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-10 21:26:33 +0200

    Square diff

--- All-git.cpp
+++ All-git.cpp
@@ -1,26 +1,22 @@
-// Number of common divisors 
+// Square diff 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int gccd(int a, int b){
-    if (!b) return a;
-    return gccd(b, a%b);
-}
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(0);cin.tie(0);
     int n; cin >> n;
+    auto p = [&](ll x){
+        if (x < 1 || x == 4) return false;
+        if (x == 2 || x == 3) return true;
+        if (!(x%2) || !(x%3)) return false;
+        for (ll i = 5; i*i<=x; i+=2) if (!(x%i)) return false;
+        return true;
+    };
     while(n--){
-        int a, b; cin >> a >> b;
-        a = gccd(a, b);
-        int res = 0;
-        for (int i = 1; i*i<=a; ++i){
-            if (!(a%i)){
-                res++;
-                if (a/i != i) res++;
-            }
-        }
-        cout << res << '\n';
+        ll a, b; cin >> a >> b;
+        if (a-b == 1 && p(a+b)) cout << "YES";
+        else cout << "NO";
+        cout << '\n';
     }
     return 0;
 }

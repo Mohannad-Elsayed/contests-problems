eab0f1861f8e63fcfff7e2200ca8f7639b21d242
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-10 17:18:13 +0200

    A. Almost Prime

--- All-git.cpp
+++ All-git.cpp
@@ -1,69 +1,25 @@
-// C. Hossam and Trainees
-#include <bits/stdc++.h>
+// A. Almost Prime
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int slv(){
-    int n, tmp; 
-    cin >> n;
-    int N = 31700;
-    vector<int> primes;
-    vector<bool> siev(N, true);
-    for (int i = 2; i*i<=N; i++){
-        if (siev[i]){
-            for (int j = i*i; j<=N; j+=i) siev[j] = false;
-        }
-    }
-    for (int i = 2; i<=N; ++i) if (siev[i]) primes.push_back(i);
-    // for (auto x : primes) cout << x << ' ';
-    unordered_set<int> uste; unordered_map<int, int> mp;
-    int arr[n];
-    for (int i = 0; i<n; ++i){cin >> arr[i];}
-
-    for (auto x : arr){
-        bool ent = false;
-        for (auto p : primes){
-            if(!(x%p)){
-                ent = true;
-                mp[p]++;
-            }
-        }
-        if (!ent) mp[x]++;
-    }
-
-    for (auto [x, y] : mp) if (y > 1) {return (cout << "YES"), 0;}
-
-    // for (int i = 0; i<n; ++i){
-    //     if (mp[arr[i]] > 1) return (cout << "YES"), 0;
-    //     for (auto x : primes){
-    //         if (!(arr[i]%x)){
-    //             if (uste.count(x)){
-    //                 // cout << arr[i] << ' ' << x << '\n';
-    //                 return (cout << "YES"), 0;
-    //             }
-    //             uste.insert(x);
-    //         }
-    //     }
-    // }
-    cout << "NO";
-    return 0;
-}
-
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    int t = 1;
-    cin >> t;
-    while(t--){
-        slv();
-        cout << '\n';
+    int n; cin >> n;
+    int N = 3010;
+    vector<int> v(N);
+    for (int i = 2; i<N; i++){
+        int tmpi = i;
+        set<int> p;
+        for (int j = 2; j*j<=tmpi; j++){
+            if (tmpi%j) continue;
+            p.insert(j);
+            while(!(tmpi%j)) tmpi /= j;
+        }
+        if (tmpi != 1) p.insert(tmpi);
+        if (p.size() == 2){
+            v[i] = 1;
+            // cout << i << '\n';
+        }
     }
+    for (int i = 1; i<3010; i++) v[i] += v[i-1];
+    cout << v[n];
 }
-
-/*
-
-2
-3
-32 48 7
-3
-14 5 9
-
-*/

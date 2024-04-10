f252bc27b208a5f03b218c94e54632fa17e95056
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-10 13:26:01 +0200

    tmp

---
+++ Codeforces/Ahmed Adel/STLs/Week 2/Sheet/F-Interesting_Function.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define forn(i, a, b) for( ll (i) = (ll)(a); (i)<(ll)(b); i++)
+
+void slv(){
+    
+}
+int main(){
+    int n = 1; 
+    cin >> n;
+    forn(i, 1, n+1){
+        slv();
+        cout << '\n';
+    }
+    return 0;
+}

46432a0dc8b9cdd4e51121ed929536ea4799a458
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-10 17:27:54 +0200

    B. Regular Bracket Sequence

--- All-git.cpp
+++ All-git.cpp
@@ -1,25 +1,21 @@
-// A. Almost Prime
+// B. Regular Bracket Sequence
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 int main(){
-    int n; cin >> n;
-    int N = 3010;
-    vector<int> v(N);
-    for (int i = 2; i<N; i++){
-        int tmpi = i;
-        set<int> p;
-        for (int j = 2; j*j<=tmpi; j++){
-            if (tmpi%j) continue;
-            p.insert(j);
-            while(!(tmpi%j)) tmpi /= j;
+    string s; cin >> s;
+    int n = s.size();
+    int cnt = 0;
+    stack<char> stk;
+    for (auto ch : s){
+        if (ch == '('){
+            stk.push(ch);
         }
-        if (tmpi != 1) p.insert(tmpi);
-        if (p.size() == 2){
-            v[i] = 1;
-            // cout << i << '\n';
+        else{
+            if (stk.empty()) cnt++;
+            else stk.pop();
         }
     }
-    for (int i = 1; i<3010; i++) v[i] += v[i-1];
-    cout << v[n];
+    cout << n-(cnt + stk.size());
+    return 0;
 }

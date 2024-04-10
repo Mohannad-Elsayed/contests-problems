d7dff8794fc788afc33df22fdfb0d4f17e68afb0
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-10 13:53:23 +0200

    single file for all problems

---
+++ All-git.cpp
@@ -0,0 +1,16 @@
+// C. Hossam and Trainees
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void slv(){
+
+}
+
+int main(){
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        slv();
+        cout << '\n';
+    }
+}
--- Codeforces/Ahmed Adel/STLs/Week 2/Sheet/F-Interesting_Function.cpp
+++ Codeforces/Ahmed Adel/STLs/Week 2/Sheet/F-Interesting_Function.cpp
@@ -1,17 +1,37 @@
+// F. Interesting Function
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define forn(i, a, b) for( ll (i) = (ll)(a); (i)<(ll)(b); i++)
-
 void slv(){
-    
+    ll l, r, resl = 0, resr = 0; cin >> l >> r;
+   for (int i = 0; i < 11; i++){
+    resl += (l/(ll)pow(10, i));
+    resr += (r/(ll)pow(10, i));
+    // l /= 10;
+    // cout << resl << '\n';
+   }
+    cout << resr-resl;
 }
 int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
     int n = 1; 
     cin >> n;
-    forn(i, 1, n+1){
+    for (int i = 1; i<=n; i++){
         slv();
         cout << '\n';
     }
     return 0;
 }
+/*
+4
+1 9
+9 10
+10 20
+1 1000000000
+
+8
+2
+11
+1111111110
+
+*/

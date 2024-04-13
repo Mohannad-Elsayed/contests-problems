777f40662e597a75eae49f1abfcbf98ed7ec1fb2
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-13 15:57:54 +0200

    Mountain Scenery

--- All-git.cpp
+++ All-git.cpp
@@ -1,67 +1,33 @@
-// The Seasonal War || UVA - 352
+// Mountain Scenery
 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-int n;
-char arr[25][25];
-void makez(int x, int y){
-    int indx = x, indy = y;
-    for (int i = 0; i<8; i++){
-        if (indx+dx[i] >= n || indx+dx[i] < 0 || indy+dy[i] >= n || indy+dy[i] < 0) continue;
-        if (arr[indx+dx[i]][indy+dy[i]] == '1'){
-            arr[indx+dx[i]][indy+dy[i]] = '0';
-            // cout << indx+dx[i] << ' ' << indy+dy[i] << '\n';
-            makez(indx+dx[i], indy+dy[i]);
-        }
-    }
-};
 
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0); 
-    int ccnt = 1;
-    while(cin >> n){
-        int cnt = 0;
-        for (int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                cin >> arr[i][j];
-
-            }
+    int n, k; cin >> n >> k;
+    int arr[2*n+1];
+    for (int i = 0; i<2*n+1; i++) cin >> arr[i];
+    for (int i = 1; i<2*n+1, k; i+=2){
+        if (arr[i]-1 > arr[i+1] && arr[i]-1 > arr[i-1]){
+            arr[i]--;
+            k--;
         }
-        for (int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if (arr[i][j] == '1'){
-                    arr[i][j] = '0';
-                    cnt++;
-                    makez(i, j);
-                }
-            }
-        }
-        printf("Image number %d contains %d war eagles.\n", ccnt, cnt);
-        ccnt++;
     }
+    for (auto x : arr) cout << x << ' ';
     return 0;
 }
 
 /*
 
-6
-100100
-001010
-000000
-110000
-111000
-010100
-8
-01100101
-01000001
-00011000
-00000010
-11000011
-10100010
-10000001
-01100000
+3 2
+0 5 3 5 1 5 2
+
+1 1
+0 2 0
+
 
 */

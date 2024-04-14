529cc64e4ccb55c77b74ac501caf578726f70ae2
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-14 21:34:06 +0200

    A. Ksenia and Pan Scales

--- All-git.cpp
+++ All-git.cpp
@@ -1,15 +1,18 @@
-// A - Chewbaсca and Number
+// A. Ksenia and Pan Scales
 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 int main(){
     // ios_base::sync_with_stdio(0);cin.tie(0); 
-    string s; cin >> s;
-    for (int i = 0; i<s.size(); i++){
-        if (!i && s[i] == '9') continue;
-        s[i] = min(s[i], (char)('9'-s[i] + '0')); 
+    string ab, a, b, extra; cin >> ab >> extra;
+    int pos = ab.find('|');
+    a = ab.substr(0, pos);
+    b = ab.substr(pos+1);
+    for (auto ch : extra){
+        if (a.size() < b.size()) a+= ch;
+        else b += ch;
     }
-    cout << s;
+    cout << (a.size() == b.size() ? a + '|' + b : "Impossible");
     return 0;
 }

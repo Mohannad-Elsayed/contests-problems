e7cd9dbb5dca323e44a030492a11ec615b6840a6
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-14 21:51:46 +0200

    A. Polo the Penguin and Segments

--- All-git.cpp
+++ All-git.cpp
@@ -1,24 +1,17 @@
-// A. Launch of Collider
+// A. Polo the Penguin and Segments 
 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 int main(){
     // ios_base::sync_with_stdio(0);cin.tie(0); 
-    int n; cin >> n;
-    string s; cin >> s;
-    int arr[n], nowr, res = INT_MAX; bool flag = 0;
-    for (auto &e : arr) cin >> e;
-    for (int i = 0; i<n; i++){
-        char ch = s[i];
-        if (ch == 'R'){
-            flag = 1;
-            nowr = arr[i];
-        }
-        if (flag && ch == 'L'){
-            res = min(res, abs(nowr- arr[i])/2);
-        }
+    int n, k; cin >> n >> k;
+    int diff = 0;
+    while(n--){
+        int l, r;
+        cin >> l >> r;
+        diff += (r-l)+1;
     }
-    cout << (res == INT_MAX ? -1 : res);
+    cout << (((diff+k-1)/k)*k)-diff;
     return 0;
 }

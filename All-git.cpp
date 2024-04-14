1160ed8eb50f10371814dc46904689776ee703c6
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-04-14 21:45:23 +0200

    A. Launch of Collider

--- All-git.cpp
+++ All-git.cpp
@@ -1,18 +1,24 @@
-// A. Ksenia and Pan Scales
+// A. Launch of Collider
 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 int main(){
     // ios_base::sync_with_stdio(0);cin.tie(0); 
-    string ab, a, b, extra; cin >> ab >> extra;
-    int pos = ab.find('|');
-    a = ab.substr(0, pos);
-    b = ab.substr(pos+1);
-    for (auto ch : extra){
-        if (a.size() < b.size()) a+= ch;
-        else b += ch;
+    int n; cin >> n;
+    string s; cin >> s;
+    int arr[n], nowr, res = INT_MAX; bool flag = 0;
+    for (auto &e : arr) cin >> e;
+    for (int i = 0; i<n; i++){
+        char ch = s[i];
+        if (ch == 'R'){
+            flag = 1;
+            nowr = arr[i];
+        }
+        if (flag && ch == 'L'){
+            res = min(res, abs(nowr- arr[i])/2);
+        }
     }
-    cout << (a.size() == b.size() ? a + '|' + b : "Impossible");
+    cout << (res == INT_MAX ? -1 : res);
     return 0;
 }
